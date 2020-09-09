6f3d2e93bcce3060da47850035969a7d77fbcf86
islom0703 <66205811+islom0703@users.noreply.github.com>
2020-09-09 16:39:55 +0500

    some changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-// import React    from 'react';
-// import ReactDOM  from 'react-dom';
-// import App from "./components/app";
-// // import '../node_modules/font-awesome/css/font-awesome.min.css';
-//
-//
-// ReactDOM.render(<App/>,
-//     document.getElementById('root'));
+import React    from 'react';
+import ReactDOM  from 'react-dom';
+import App from "./components/app";
+// import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 
-fetch('https://swapi.dev/api/people/1/')
-    .then((res) => {
-        return res.json();
-    })
-    .then((body) =>{
-        console.log(body)
-    });
+ReactDOM.render(<App/>,
+    document.getElementById('root'));
